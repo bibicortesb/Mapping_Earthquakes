27782d0f4ee799746ad7598b8fdda7cb7418f7c1
Bibi Cortes <bibicortes@Bibis-MacBook-Air.local>
2023-03-27 15:31:57 -0600

    circle marker

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -11,7 +11,7 @@ let cityData = cities;
 cityData.forEach(function(city) {
     console.log(city)
     L.circlemarker(city.location,{
-        radius: city.population
+        city.population/100000
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
